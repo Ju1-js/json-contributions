27e97b0caa833b2181eb062babb4bed2657536f0
Martin Woodward <martinwoodward@github.com>
2020-12-16 08:43:14 +0000

    Add distribution calculations

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Given a Github username and a year, return a JSON object with their contribution chart data. The function simply requests the user's profile page and scrapes the contribution chart data.
+Given a Github username and a year, return a JSON object with their contribution chart data. The function simply requests the user's profile page and scrapes the contribution chart data. Based on the work of [@jasonlong](https://github.com/jasonlong)
 
 **Note: this is very alpha. No error checking at all.**
 
@@ -6,8 +6,8 @@ Given a Github username and a year, return a JSON object with their contribution
 /api/user?username=<username>&year=<year>
 ```
 
-Example: https://json-contributions.vercel.app/api/user?username=jasonlong&year=2018
+Example: https://json-contributions-five.vercel.app/api/user?username=martinwoodward&year=2020
 
 #### Development
 
-I've been using `vercel` for local dev. To load up a local server: `vercel dev`.
+I've been using `vercel` for local dev. To load up a local server: `vercel dev` after running `npm install`
--- api/user.js
+++ api/user.js
@@ -8,8 +8,14 @@ export default (req, res) => {
     year = req.query.year
   }
   let contributions = []
-  let minCount = 0
+  let minCount = 1000
   let maxCount = 0
+  let median = 0
+  let percentile80 = 0
+  let percentile90 = 0
+  let percentile99 = 0
+
+  let nonZeroValues = []
 
   let url = `${req.query.username}?tab=overview&from=${year}-12-01&to=${year}-12-31`
   got(url, {prefixUrl: GH_DOMAIN}).then(response => {
@@ -22,25 +28,62 @@ export default (req, res) => {
       week['days'] = []
       $(w).find('rect').each((j, r) => {
         let day = {}
+        let count = $(r).data('count')
         day['date'] = $(r).data('date')
-        day['count'] = $(r).data('count')
+        day['count'] = count
         week['days'].push(day)
 
-        if ($(r).data('count') <= minCount) {
-          minCount = $(r).data('count')
+        if (count > 0)
+        {
+          nonZeroValues.push(count)
+          if (count <= minCount) {
+            minCount = count
+          }
         }
-        if ($(r).data('count') > maxCount) {
-          maxCount = $(r).data('count')
+
+        if (count > maxCount) {
+          maxCount = count
         }
       })
       contributions.push(week)
     })
+
+    // If we've only had 0's returned
+    if (maxCount === 0)
+    {
+      minCount = 0
+    }
+
+    // Calculate distributions
+    if(nonZeroValues.length > 0)
+    {
+      nonZeroValues.sort(function(a,b){
+        return a-b;
+      });
+  
+      var half = Math.floor(nonZeroValues.length / 2);
+      if (nonZeroValues.length % 2)
+      {
+        median = nonZeroValues[half]
+      }
+      else {
+        median = (nonZeroValues[half - 1] + nonZeroValues[half]) / 2.0
+      }
+
+      percentile80 = (nonZeroValues[Math.floor(nonZeroValues.length * 0.8) - 1] + nonZeroValues[Math.floor(nonZeroValues.length * 0.8)]) / 2.0
+      percentile90 = (nonZeroValues[Math.floor(nonZeroValues.length * 0.9) - 1] + nonZeroValues[Math.floor(nonZeroValues.length * 0.9)]) / 2.0
+      percentile99 = (nonZeroValues[Math.floor(nonZeroValues.length * 0.99) - 1] + nonZeroValues[Math.floor(nonZeroValues.length * 0.99)]) / 2.0
+    }
 
     res.json({
       username: req.query.username,
       year: year,
       min: minCount,
       max: maxCount,
+      median: median,
+      p80: percentile80,
+      p90: percentile90,
+      p99: percentile99,
       contributions: contributions
     })
   }).catch(err => {
